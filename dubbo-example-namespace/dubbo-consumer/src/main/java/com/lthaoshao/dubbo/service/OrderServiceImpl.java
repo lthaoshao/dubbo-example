d0fe787cbefd1e3e97a9decd395a578593b9f851
李景浩 <lijinghao@u51.com>
2019-07-23 17:24:54 +0800

    关于AutoWired使用的问题.

--- dubbo-example-namespace/dubbo-consumer/src/main/java/com/lthaoshao/dubbo/service/OrderServiceImpl.java
+++ dubbo-example-namespace/dubbo-consumer/src/main/java/com/lthaoshao/dubbo/service/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import com.lthaoshao.dubbo.request.BaseRequest;
 import com.lthaoshao.dubbo.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
  * <p>  </p>
@@ -18,7 +17,8 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements IOrderService {
 
     @Autowired
-    private TradeServiceFacade tradeServiceFacade;
+    private TradeServiceFacade tradeService;
+
 
     public OrderServiceImpl() {
     }
@@ -29,7 +29,7 @@ public class OrderServiceImpl implements IOrderService {
         BaseRequest request = new BaseRequest();
         request.setRequestId(String.valueOf(System.currentTimeMillis()));
 
-        BaseResponse result = tradeServiceFacade.createOrder(request);
+        BaseResponse result = tradeService.createOrder(request);
 
         log.info("response:{}", result);
         return result.getMsg();
